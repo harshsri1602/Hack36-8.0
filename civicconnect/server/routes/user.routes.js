b10775e6d28440ae6b199103e65b7160e63430af
Harsh Srivastava <harshsri1602@gmail.com>
2025-04-19 14:23:04 +0530

    added routes for viewing all posts and deleting a post

--- civicconnect/server/routes/user.routes.js
+++ civicconnect/server/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { userLogin, userRegister } from '../controllers/user/user.authController.js';
-import { createPost } from '../controllers/user/user.postModelController.js';
+import { createPost , viewAllUserPosts , deletePost} from '../controllers/user/user.postModelController.js';
 import authUser from '../middleware/authUser.js';
 const UserRouter= express.Router();
 
 UserRouter.post('/register',userRegister);
 UserRouter.post('/login',userLogin);
 UserRouter.post('/postIssue',authUser,createPost);
-
+UserRouter.get('/viewAllPosts',authUser,viewAllUserPosts);
+UserRouter.delete('/deletePost/:postId',deletePost);
 export default UserRouter
