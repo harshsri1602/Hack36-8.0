427fd96e02d03e6f30aff0bef1bb3a7e0b058554
Harsh Srivastava <127097504+harshsri1602@users.noreply.github.com>
2025-04-19 15:23:44 +0530

    Delete CivicConnect directory

--- CivicConnect/utils/postCard.tsx
+++
@@ -1,8 +0,0 @@
-export interface PostCardProps {
-    id: string;
-    title: string;
-    descriptionText?: string;
-    descriptionImageSrc?: string;
-    commentsCount: number;
-    status: string;
-}
